5abcc1b8bea198f20270691b94c6f78146bcff25
gregkang <greg_kang@hotmail.com>
2017-03-27 10:41:23 -0700

    Story 1 iteration 2

--- src/main/java/com/ebaby/application/User.java
+++ src/main/java/com/ebaby/application/User.java
@@ -3,8 +3,33 @@ package com.ebaby.application;
 public class User {
     private String firstName;
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String lastName;
+    private String userEmail;
+    private String userName;
+    private String password;
+
     public User(String firstName, String lastName, String userEmail, String userName, String password) {
         this.firstName = firstName;
+        this.lastName = lastName;
+        this.userEmail = userEmail;
+        this.userName = userName;
+        this.password = password;
     }
 
     public String getFirstName() {
@@ -13,5 +38,33 @@ public class User {
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean doRegister(){
+        return false;
+    }
+
+    public boolean login(){
+        return false;
+    }
+
+    public boolean logout(){
+        return false;
     }
 }
--- src/test/java/com/ebaby/application/UserTest.java
+++ src/test/java/com/ebaby/application/UserTest.java
@@ -7,15 +7,54 @@ import org.junit.Test;
 public class UserTest {
 
     private String firstName = "firstName";
+    private String lastName = "lastName";
+    private String userEmail = "userEmail";
+    private String userName = "userName";
+    private String password = "password";
     private User testUser;
 
     @Before
     public void setup() {
-        testUser = new User(firstName, "lastName", "userEmail", "userName", "password");
+        testUser = new User(firstName, lastName, userEmail, userName, password);
     }
 
     @Test
-    public void createUser() {
+    public void checkFirstName() {
         Assert.assertEquals(firstName, testUser.getFirstName());
+    }
+
+    @Test
+    public void checkLastNAme(){
+        Assert.assertEquals(lastName, testUser.getLastName());
+    }
+
+    @Test
+    public void checkUserEmail(){
+        Assert.assertEquals(userEmail, testUser.getUserEmail());
+    }
+
+    @Test
+    public void checkUserName(){
+        Assert.assertEquals(userName, testUser.getUserName());
+    }
+
+    @Test
+    public void checkPassword(){
+        Assert.assertEquals(password, testUser.getPassword());
+    }
+
+    @Test
+    public void tryRegistering(){
+        Assert.assertTrue("Register returned false", testUser.doRegister());
+    }
+
+    @Test
+    public void tryLoginSuccess(){
+        Assert.assertTrue("Login must be successful", testUser.login());
+    }
+
+    @Test
+    public void tryLogoutSuccess(){
+        Assert.assertTrue("Logout must be successful",testUser.logout());
     }
 }
